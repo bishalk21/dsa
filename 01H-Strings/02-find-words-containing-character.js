64d2a3b1b546bb49626df6f198c5ddd0728e3a33
bishalk21 <karkibishal00@gmail.com>
2025-10-07 09:45:59 +1100

    find words containting character problem

---
+++ 01H-Strings/02-find-words-containing-character.js
@@ -0,0 +1,43 @@
+/** Find Words Containing Character
+ * https://leetcode.com/problems/find-words-containing-character/description/
+ *
+ * You are given a 0-indexed array of strings words and a character x.
+ * Return an array of indices representing the words that contain the character x.
+ * Note that the returned array may be in any order.
+ *
+ * Example 1:
+ * Input: words = ["leet","code"], x = "e"
+ * Output: [0,1]
+ * Explanation: "e" occurs in both words: "leet", and "code". Hence, we return indices 0 and 1.
+ *
+ * Example 2:
+ * Input: words = ["abc","bcd","aaaa","cbc"], x = "a"
+ * Output: [0,2]
+ * Explanation: "a" occurs in "abc", and "aaaa". Hence, we return indices 0 and 2.
+ */
+
+// using built-in functions
+function findWordsContainingCharacter(words, x) {
+  let result = [];
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].includes(x)) {
+      result.push(i);
+    }
+  }
+  return result;
+}
+
+// without using built-in functions
+function findWords(words, x) {
+  let result = [];
+
+  for (let i = 0; i < words.length; i++) {
+    for (let j = 0; j < words[i].length; j++) {
+      if (words[i][j] === x) {
+        result.push(i);
+        break; // No need to check further in this word
+      }
+    }
+  }
+  return result;
+}
