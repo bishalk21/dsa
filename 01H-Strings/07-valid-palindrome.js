2605c0f0113e4b713fea20d2f6576fb500cc99ce
bishalk21 <karkibishal00@gmail.com>
2025-10-14 10:42:18 +1100

    valid palidrome using two pointers

--- 01H-Strings/07-valid-palindrome.js
+++ 01H-Strings/07-valid-palindrome.js
@@ -25,7 +25,19 @@
  * - time complexity: O(n)
  * - space complexity: O(n)
  *
- * Algorithm: using built-in functions
+ * Algorithm: two pointers - no extra space
+ * 1. Convert the string to lowercase.
+ * 2. Initialize two pointers, left and right, at the start and end of the string.
+ * 3. Move the left pointer to the right until it points to an alphanumeric character.
+ * 4. Move the right pointer to the left until it points to an alphanumeric character.
+ * 5. Compare the characters at the left and right pointers.
+ * 6. If they are the same, move both pointers towards the center and repeat steps 3-5.
+ * 7. If they are not the same, return false.
+ * 8. If the left pointer is greater than or equal to the right pointer, return true.
+ * 9. Time Complexity: O(n), where n is the length of the string.
+ * 10. Space Complexity: O(1) since we are not using any extra space.
+ *
+ * Algorithm: using built-in functions - extra space
  * 1. Convert the string to lowercase.
  * 2. Filter out non-alphanumeric characters.
  * 3. Reverse the filtered string.
@@ -33,9 +45,37 @@
  * 5. Return true if they are the same, else return false.
  * 6. Time Complexity: O(n), where n is the length of the string.
  * 7. Space Complexity: O(n) for the filtered string and reversed string.
- *
  * Also: can add the characters to the reversed string while filtering
  */
+
+function isPalindrome(s) {
+  // lowercase the string
+  s = s.toLowerCase();
+  let left = 0;
+  let right = s.length - 1;
+
+  // two pointers check if the length is a palindromes
+  while (left < right) {
+    // if left is not alphanumeric, move left pointer to the right
+    if (!s[left].match(/[a-z0-9]/i)) {
+      ++left;
+    }
+    // if right is not alphanumeric, move right pointer to the left
+    else if (!s[right].match(/[a-z0-9]/i)) {
+      --right;
+    }
+    // if both are alphanumeric, check if they are the same
+    else if (s[left] === s[right]) {
+      ++left;
+      --right;
+    }
+    // if they are not the same, return false
+    else if (s[left] !== s[right]) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function isValidPalindrome(s) {
   // filter out non-alphanumeric characters and convert to lowercase
